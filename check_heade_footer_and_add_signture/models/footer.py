0a2c11fd2f42cf917740e61b4704a4b7e091335e
Your Name <you@example.com>
2021-07-16 01:05:07 +0200

    [MOD]modifiy headerfooter

--- check_heade_footer_and_add_signture/models/footer.py
+++ check_heade_footer_and_add_signture/models/footer.py
@@ -2,9 +2,18 @@
 from odoo import models, fields, api
 
 
-class CompanyInherit(models.Model):
-    _inherit = 'res.company'
-    _description = 'CompanyInherit'
+# class CompanyInherit(models.Model):
+#     _inherit = 'res.company'
+#     _description = 'CompanyInherit'
+#
+#     is_without_footer_header = fields.Boolean("Without Footer And Header")
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
     is_without_footer_header = fields.Boolean("Without Footer And Header")
 
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    is_without_footer_header = fields.Boolean("Without Footer And Header")
