c5a08499d39c6f179aca234dff8658e21cd505d1
ayah <ayahashraf236@gmail.com>
2021-10-30 02:54:32 +0200

    [MOD] Updates

--- website_elnoman/static/src/js/script.js
+++ website_elnoman/static/src/js/script.js
@@ -28,3 +28,25 @@ function directTel(){
 function directEmail(){
     window.open('mailto:info@elnomangroup.com');
 }
+
+$(function() {
+    'use strict';
+    $('.langs_dropdown').on('click', function() {
+        $(this).find('ul').toggle();
+    });
+    $('body').on('click', '.o_header_affix .langs_dropdown, .o_header_affix .langs_dropdown ul', function() {
+        $(this).find('ul').toggle();
+    });
+    $(document).on('click', function() {
+        $('.nav_menu_language').hide();
+    });
+    $(document).on('click', '.o_header_affix .langs_dropdown .nav_menu_language', function() {
+        $(this).hide();
+    });
+    $('.langs_dropdown').on('click', function(e) {
+        e.stopPropagation();
+    });
+    $('body').on('click', '.o_header_affix .langs_dropdown', function(e) {
+        e.stopPropagation();
+    });
+})
