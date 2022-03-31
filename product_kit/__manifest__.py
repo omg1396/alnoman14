4a1eb85fe15df3e507a32dc3fb8f0b3b6c8774ef
Your Name <you@example.com>
2022-03-31 11:21:12 +0200

    [MOD]

--- product_kit/__manifest__.py
+++ product_kit/__manifest__.py
@@ -19,6 +19,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/forms/product_product.xml',
